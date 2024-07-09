8a33a2b5491a4657256cba8d84b24735016765f9
biruk <birukendalew090@gmail.com>
2024-07-09 19:27:26 +0300

    add onboarding page

--- lib/pages/onboarding_page.dart
+++ lib/pages/onboarding_page.dart
@@ -6,50 +6,57 @@ class OnboardingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: const Color(0xff2C2B34),
       body: Column(
         children: [
-          Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/onboarding.png'),
-                    fit: BoxFit.cover)),
+          Expanded(
+            flex: 2,
+            child: Container(
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/onboarding.png'),
+                      fit: BoxFit.cover)),
+            ),
           ),
-          Container(
-            child: Column(
-              children: [
-                const Text(
-                  'Premium Cars \nEnjoy The Luxery',
-                  style: TextStyle(
-                      color: Colors.black54,
-                      fontSize: 34,
-                      fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Text(
-                  'Premium and prestige car daily rental. \nExperience the thrill at a lower price',
-                  style: TextStyle(color: Colors.grey, fontSize: 16),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                  width: 320,
-                  height: 54,
-                  child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.black,
-                          backgroundColor: Colors.white),
-                      child: const Text(
-                        'Let\'s Go',
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                      )),
-                )
-              ],
+          Expanded(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Premium Cars \nEnjoy The Luxery',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 34,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    'Premium and prestige car daily rental. \nExperience the thrill at a lower price',
+                    style: TextStyle(color: Colors.grey, fontSize: 16),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: 320,
+                    height: 54,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.black,
+                            backgroundColor: Colors.white),
+                        child: const Text(
+                          'Let\'s Go',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        )),
+                  )
+                ],
+              ),
             ),
           )
         ],
